27ddb4019216a00798b9095f6a3fb539177d7d68
Abhishek <abhikharpal@gmail.com>
2023-01-16 12:27:31 +0530

    feat: longestStringReg implemented using ternary

--- longestString.js
+++ longestString.js
@@ -2,14 +2,13 @@ function longestStringReg(vals){
     let res="";
     let curr=0;
     vals.forEach((element)=>{
-        if(element.length>curr){
-            curr=element.length;
-            res=element
-        }
+        res=(element.length===curr)?((element>res)?res:element):res
+        res = (element.length>curr)?element:res;
+        curr=res.length
     })
     return res;
 }
-
+//console.log(longestStringReg(['abc','ced','aaa']))
 const longestStringArrow=(vals)=>{
     let res="";
     let curr=0;
